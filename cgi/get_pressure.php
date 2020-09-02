bda015caf52a5bd2a0cffebd6760f9767928503b
root <romspam@wiro.fr>
2020-09-02 10:12:22 +0100

    tmp workaround

--- cgi/get_pressure.php
+++ cgi/get_pressure.php
@@ -10,10 +10,21 @@ if (array_key_exists('dt', $_GET) && preg_match('#^\d{8}$#', $_GET['dt'])) {
 }
 
 if ($dt != "") {
-    $cmd = "curl -s https://modeles.meteociel.fr/modeles/gfs/archives/gfs-".$dt."00-0-12.png | gdal_translate -of GTiff -a_ullr -61 80 39 25.5 -a_srs EPSG:4269 /vsistdin/ /vsistdout/ | gdalwarp -t_srs EPSG:3857 /vsistdin/ /vsistdout/ | convert - png:-";
-    # error handling ? not interesting.
+    #echo "aaa";
+    #echo "curl -s https://modeles.meteociel.fr/modeles/gfs/archives/gfs-".$dt."00-0-12.png | gdal_translate -of GTiff -a_ullr -61 80 39 25.5 -a_srs EPSG:4269 /vsistdin/ $file_imed";
     header('Content-Type: image/png');
     header('Access-Control-Allow-Origin: *');
+    #exit();
+
+    $file_in = "/tmp/pres_in_file_".rand(1,10000).".tif";
+    $file_imed = "/tmp/pres_imed_file_".rand(1,10000).".tif";
+    $file_out  = "/tmp/pres_out_file_".rand(1,10000).".tif";
+    exec("curl -s https://modeles.meteociel.fr/modeles/gfs/archives/gfs-".$dt."00-0-12.png | convert - tif:$file_in ; gdal_translate -of GTiff -a_ullr -61 80 39 25.5 -a_srs EPSG:4269 $file_in $file_imed 2> /dev/null");
+    exec("gdalwarp -t_srs EPSG:3857 $file_imed $file_out ");
+    passthru("convert tif:$file_out png:-");
+    exit();    
+    $cmd = "curl -s https://modeles.meteociel.fr/modeles/gfs/archives/gfs-".$dt."00-0-12.png | gdal_translate -of GTiff -a_ullr -61 80 39 25.5 -a_srs EPSG:4269 /vsistdin/ /vsistdout/ | gdalwarp -t_srs EPSG:3857 /vsistdin/ /vsistdout/ | convert - png:-";
+    # error handling ? not interesting.
     passthru($cmd);
 }
 
