e00392b0fae14808fdffe65fd9f04c3e0216103c
root <romspam@wiro.fr>
2021-03-16 19:32:06 +0000

    upgrade

--- cgi/update.php
+++ cgi/update.php
@@ -1,9 +1,10 @@
 <?php
 
 $token = file_get_contents ( "/var/www/token");
-
-if (!strcmp ($token, "") && $_GET["token"] == $token) {
-    passthru("cd /var/www/cfd_acc && git pull");
+if ($token != "" && $_GET["token"] == $token) {
+    echo "[+] starting pull";
+    passthru("cd /var/www/cfd_acc && git pull 2>&1; ");
+    echo "[+] Might be done";
 }
 
 ?>
