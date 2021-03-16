55406325e194e6de7edf3c8ef873a6adab6afd8b
root <romspam@wiro.fr>
2021-03-16 19:52:13 +0000

    upgrade

--- cgi/update.php
+++ cgi/update.php
@@ -3,7 +3,12 @@
 $token = file_get_contents ( "/var/www/token");
 if ($token != "" && $_GET["token"] == $token) {
     echo "[+] starting pull";
-    passthru("cd /var/www/cfd_acc && git pull 2>&1; ");
+    if ($_SERVER['SERVER_NAME'] == "cfd.acc.wiro.fr") {
+        passthru("cd /var/www/cfd_acc && git pull 2>&1; ");
+    }
+    else if ($_SERVER['SERVER_NAME'] == "cfd.wiro.fr") {
+        passthru("cd /var/www/cfd_explorer && git pull 2>&1; ");
+    }
     echo "[+] Might be done";
 }
 
